e4c9403c617d5d978dc2dad8b1e94fed642abc19
ihgazni2 <286264978@qq.com>
2018-06-20 01:14:07 +0800

    remove old build

--- nvhead/specific.py
+++ nvhead/specific.py
@@ -1,4 +1,10 @@
 from nvhead.consts import *
 from nvhead.common import *
 
-Accept = TypeSubtypeQ
+
+class Accept(TypeSubtypeQ):
+    def __init__(self,one,**kwargs):
+        super(Accept,self).__init__(one,**kwargs)
+        self.header_type = "req"
+        self.forbidden_header_name = False
+        self.cros_safelisted_request_header = True
